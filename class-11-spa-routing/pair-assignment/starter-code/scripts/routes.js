f9519f9c1bbbed42958bc7b478be8cf87fcf87cb
Jeremy Edwards <jeremytedwards@gmail.com>
2016-02-16 09:40:50 -0800

    In class work to learn page.js and NodeJS

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,10 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
+page('/', articlesController.index);
+page('/about', aboutController.index);
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+
+// DONE: What function do you call to activate page.js? Fire it off now, to execute
+page();
+
+// page({}); // look at the options example #hashbang
