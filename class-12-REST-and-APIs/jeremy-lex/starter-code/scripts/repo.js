f2639b9953651ab0ab19d10e53eb93bbcd6d0864
Lex Myers <lex.myers@gmail.com>
2016-02-16 14:52:48 -0800

    moved code to load repo data from githubToken.js to repo.js

--- class-12-REST-and-APIs/jeremy-lex/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/jeremy-lex/starter-code/scripts/repo.js
@@ -5,22 +5,18 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-    console.log('gitAJAX: ' + $gitTokenAJAX);
-    $.ajax($gitTokenAJAX).done(callback);
-
-    // var $gitTokenAJAX = {
-    //   url: 'https://api.github.com/users/jeremytedwards/repos?per_page=5&sort=updated',
-    //   type: 'GET',
-    //   headers: { 'Authorization': 'token ' + githubToken },
-    //   success: function(repoData, message, xhr) {
-    //     console.log('Processing each Repo' + repoData);
-    //     repoData.forEach( function(xhr) {
-    //       repos.with(xhr);
-    //     });
-    //   } // end of success function
-    // };//end of gitTokenAJAX object
-
-  };
+    $.ajax({
+        url: 'https://api.github.com/users/' + githubUser + '/repos?per_page=5&sort=updated',
+        type: 'GET',
+        headers: { 'Authorization': 'token ' + githubToken },
+        success: function(repoData, message, xhr) {
+          console.log('Processing each Repo' + repoData);
+          repos.all = $.map(repoData, function(el) {
+            return el;
+          });
+        }
+      }).done(callback);
+    };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
